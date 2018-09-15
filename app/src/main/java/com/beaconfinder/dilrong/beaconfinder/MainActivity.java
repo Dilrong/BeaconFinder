3398a4336454f32dab52b15abdf3fc2348f676c7
dilrong <wet932@naver.com>
2018-09-15 14:15:31 +0900

    add bluetooth func

--- app/src/main/java/com/beaconfinder/dilrong/beaconfinder/MainActivity.java
+++ app/src/main/java/com/beaconfinder/dilrong/beaconfinder/MainActivity.java
@@ -7,23 +7,98 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.LocationManager;
+import android.media.Image;
 import android.os.Build;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.minew.beacon.BeaconValueIndex;
+import com.minew.beacon.BluetoothState;
+import com.minew.beacon.MinewBeacon;
+import com.minew.beacon.MinewBeaconManager;
+import com.minew.beacon.MinewBeaconManagerListener;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private static final int PERMISSION_REQUEST_COARSE_LOCATION = 456;
+    private ImageView img_state;
+    private TextView tv_state;
+    private Button btn_find;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        img_state = findViewById(R.id.img_state);
+        tv_state = findViewById(R.id.tv_state);
+        btn_find = findViewById(R.id.btn_find);
+        btn_find.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findBeacon();
+            }
+        });
+
         checkPermission();
     }
 
+    private void findBeacon(){
+        MinewBeaconManager minewBeaconManager = MinewBeaconManager.getInstance(this);
+        minewBeaconManager.setDeviceManagerDelegateListener(new MinewBeaconManagerListener() {
+            @Override
+            public void onAppearBeacons(List<MinewBeacon> minewBeacons) {
+                //3seconds callback
+                tv_state.setText("Finding Your Item...");
+                img_state.setImageResource(R.drawable.ic_finding);
+            }
 
+            @Override
+            public void onDisappearBeacons(List<MinewBeacon> minewBeacons) {
+                //1seconds callback
+                tv_state.setText("Disappearing Beacon...");
+                img_state.setImageResource(R.drawable.ic_disconnect);
+            }
+
+            @Override
+            public void onRangeBeacons(List<MinewBeacon> minewBeacons) {
+                //every 1seconds callback
+                for(MinewBeacon minewBeacon : minewBeacons){
+                    int rssi = minewBeacon.getBeaconValue(BeaconValueIndex.MinewBeaconValueIndex_RSSI).getIntValue();
+                    if(rssi > 100){
+                        tv_state.setText("Wow Find Your Item!");
+                        img_state.setImageResource(R.drawable.ic_find);
+                    }
+                }
+            }
+
+            @Override
+            public void onUpdateState(BluetoothState bluetoothState) {
+                //Bluetooth State
+                switch (bluetoothState) {
+                    case BluetoothStateNotSupported:
+                        tv_state.setText("Bluetooth State Not Supported...");
+                        img_state.setImageResource(R.drawable.ic_error);
+                        break;
+                    case BluetoothStatePowerOff:
+                        tv_state.setText("Plz PowerOn Bluetooth!");
+                        img_state.setImageResource(R.drawable.ic_disconnect);
+                        break;
+                    case BluetoothStatePowerOn:
+                        tv_state.setText("Wait...finding");
+                        img_state.setImageResource(R.drawable.ic_connect);
+                        break;
+                }
+            }
+        });
+
+        minewBeaconManager.startScan();
+    }
 
     private boolean checkPermission(){
         BluetoothManager bluetoothManager = (BluetoothManager)

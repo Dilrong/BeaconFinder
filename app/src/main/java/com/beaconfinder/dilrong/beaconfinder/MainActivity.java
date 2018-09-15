501e8da5a5f8ca010f30c15fccf729387f863462
dilrong <wet932@naver.com>
2018-09-15 13:20:27 +0900

    check Permission

--- app/src/main/java/com/beaconfinder/dilrong/beaconfinder/MainActivity.java
+++ app/src/main/java/com/beaconfinder/dilrong/beaconfinder/MainActivity.java
@@ -1,13 +1,45 @@
 package com.beaconfinder.dilrong.beaconfinder;
 
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.os.Build;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int PERMISSION_REQUEST_COARSE_LOCATION = 456;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkPermission();
+    }
+
+
+
+    private boolean checkPermission(){
+        BluetoothManager bluetoothManager = (BluetoothManager)
+                getApplication().getSystemService(getApplicationContext().BLUETOOTH_SERVICE);
+        BluetoothAdapter mBluetoothAdapter = bluetoothManager.getAdapter();
+
+        if((mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled())){
+            startActivity(new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE));
+            return false;
+        }
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if(checkSelfPermission(android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+                return false;
+            }
+        }
+        return true;
     }
 }
